01bb8f64922c2d4eed1bf0d4944c1eef49a84607
ksaqib <ksaqib.ks@gmail.com>
2020-01-25 01:04:33 -0600

    Javascript Course Step 156

---
+++ JavaScript-Projects/JS/External.js
@@ -0,0 +1,12 @@
+function countDown(){
+    let seconds = document.getElementById("seconds").value;
+    function tick(){
+        seconds = seconds -1;
+        timer.innerHTML = seconds;
+        setTimeout(tick, 1000);
+        if (seconds == -1){
+            alert("Time's up!");
+            } 
+    }
+    tick();
+}
